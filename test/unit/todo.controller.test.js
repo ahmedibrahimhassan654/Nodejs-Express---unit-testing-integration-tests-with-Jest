bcbaaac43ea5091d9cb63da99927342789dd0f7e
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-25 00:06:50 +0200

    create controller test

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -1,9 +1,17 @@
+//import controller
 const TodoController=require('../../controllers/todocontroller')
+//import model
+const TodoModel=require('../../model/todo.model')
 
-
+ TodoModel.create=jest.fn()
 describe('TodoController.createTodo',()=>{
     it('should have a createTodo function',()=>{
         expect(typeof TodoController.createTodo).toBe('function')
-
     })
+    it('should call create controller to do model ',()=>{
+
+        TodoController.createTodo()
+        expect(TodoModel.create).toBeCalled()
+    })
+
 })
