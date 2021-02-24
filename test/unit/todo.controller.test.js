00170d2d4220fb68a9ad7a5c87d3899990771c33
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-25 00:37:31 +0200

    import http mocks for test

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -2,6 +2,9 @@
 const TodoController=require('../../controllers/todocontroller')
 //import model
 const TodoModel=require('../../model/todo.model')
+//import http mocks
+const httpMocks=require('node-mocks-http')
+
 
  TodoModel.create=jest.fn()
 describe('TodoController.createTodo',()=>{
@@ -9,8 +12,11 @@ describe('TodoController.createTodo',()=>{
         expect(typeof TodoController.createTodo).toBe('function')
     })
     it('should call create controller to do model ',()=>{
-
-        TodoController.createTodo()
+        let req,res,next
+        req=httpMocks.createRequest()
+        res=httpMocks.createResponse()
+        next=null
+        TodoController.createTodo(req,res,next)
         expect(TodoModel.create).toBeCalled()
     })
 
