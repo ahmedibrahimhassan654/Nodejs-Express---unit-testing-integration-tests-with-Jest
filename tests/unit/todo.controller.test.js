5c64d6d70de151880c2651bf53ca057141f8c496
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-14 23:37:16 +0200

    9. Using jest.fn to mock our Mongoose model functions

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,8 +1,14 @@
 const TodoController = require("../../controllers/todo.contrllers");
+const TodoModel = require("../../model/Todo.model");
 
+TodoModel.create = jest.fn();
 
-describe('TodoControler.createTodo', () => {
-    it('sholuld create todo api',()=>{
-        expect(typeof TodoController.createTodo).toBe('function')
-    })
-})
+describe("TodoControler.createTodo", () => {
+  it("sholuld create todo api", () => {
+    expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should call todoModel.create", () => {
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
+  });
+});
